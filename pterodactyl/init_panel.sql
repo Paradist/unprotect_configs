620436a9f92b7099d4928488881ad488ad4fa4af
Paradist <mcraftdn@gmail.com>
2024-09-22 23:39:48 +0300

    Update configs

--- pterodactyl/init_panel.sql
+++ pterodactyl/init_panel.sql
@@ -1,8 +1,5 @@
-CREATE USER 'panel_sldwi22fjq'@'127.0.0.1' IDENTIFIED BY 'KS9osdi113ew2KL1895601KklsdoeirjlbklflskwkldfiKjdsmvls298sdklKSlklsdkslfls';
+CREATE USER 'panel_sldwi22fjq'@'%' IDENTIFIED BY 'KS9osdi113ew2KL1895601KklsdoeirjlbklflskwkldfiKjdsmvls298sdklKSlklsdkslfls';
 CREATE DATABASE panel_kaslk2oi42;
-GRANT ALL PRIVILEGES ON panel_kaslk2oi42.* TO 'panel_sldwi22fjq'@'127.0.0.1' WITH GRANT OPTION;
-CREATE USER 'for_backups_sdkks12'@'127.0.0.1' IDENTIFIED BY 'KS9osdi113ew2Kpof95601KklsdoeirjlbkllodklvnkaldpqowwwkKOSiKjdsmvls298sdklK';
-GRANT ALL PRIVILEGES ON panel_kaslk2oi42.* TO 'for_backups_sdkks12'@'127.0.0.1';
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON panel_kaslk2oi42.* TO 'panel_sldwi22fjq'@'%' WITH GRANT OPTION;
 DROP USER 'root'@'localhost';
 FLUSH PRIVILEGES;
