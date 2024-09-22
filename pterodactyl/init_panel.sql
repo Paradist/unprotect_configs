84cf7c88f384bf42f33d26329524b6db6af5a736
Paradist <mcraftdn@gmail.com>
2024-09-23 00:17:16 +0300

    Update configs

--- pterodactyl/init_panel.sql
+++ pterodactyl/init_panel.sql
@@ -2,4 +2,5 @@ CREATE USER 'panel_sldwi22fjq'@'%' IDENTIFIED BY 'KS9osdi113ew2KL1895601Kklsdoei
 CREATE DATABASE panel_kaslk2oi42;
 GRANT ALL PRIVILEGES ON panel_kaslk2oi42.* TO 'panel_sldwi22fjq'@'%' WITH GRANT OPTION;
 DROP USER 'root'@'localhost';
+DROP USER 'root'@'%';
 FLUSH PRIVILEGES;
